c7a4ca18d2ab9935c9f4d921b02656d5d1b7de8c
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2024-02-25 13:31:46 +0530

    Update start.py
    Signed-off-by: Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -26,15 +26,7 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="lood"),
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
-            
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-        ],
-        [
-            InlineKeyboardButton("ᴅᴏɴᴀᴛᴇ", callback_data="donate"),
-        ],
+            InlineKeyboardButton(text="ᴇᴍᴍᴀ ʟᴏᴠᴇ", callback_data="allrepo"),
+            ],
     ]
     return buttons
